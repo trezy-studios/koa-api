0fdb6b36bcc9c8b3de119a5089b2b7ddcc12a539
Trezy <tre@trezy.com>
2023-12-30 16:10:26 -0600

    fix(github actions): allow semantic-release to own package.json versioning

--- release.config.js
+++ release.config.js
@@ -10,6 +10,10 @@ export default {
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/github',
+    [
+      "@semantic-release/npm",
+      { "npmPublish": false }
+    ],
 		'semantic-release-export-data',
 	],
 }
