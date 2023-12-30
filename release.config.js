589c48f876506a3209c5cdd3d460b0a19ed8890d
Trezy <tre@trezy.com>
2023-12-30 15:46:19 -0600

    ci(github actions): take control of npm publishing
    semantic-release was failing to respect the `files` prop in package.json.

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	branches: [
+		'main',
+		{
+			name: 'beta',
+			prerelease: true,
+		},
+	],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'semantic-release-export-data',
+	],
+}
